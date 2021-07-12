f496b9a57ac6b225d712d29b50eb4d846be7b5fa
Malyika J <62891809+malyikaj@users.noreply.github.com>
2021-07-12 15:14:07 -0400

    Adding css,js,txt files

---
+++ question.js
@@ -0,0 +1,27 @@
+
+//Array of Objects
+
+
+
+const quiz = [
+{
+    q:'A question?',
+    options:['example1','example2','example3','example4'],
+    answer: 0
+},
+{
+    q:'A question?',
+    options:['example1','example2','example3','example4'],
+    answer: 1
+},
+{
+    q:'A question?',
+    options:['example1','example2','example3','example4'],
+    answer: 2
+},
+{
+    q:'A question?',
+    options:['true','false'],
+    answer: 3
+},
+]
